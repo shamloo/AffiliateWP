130c079d2bc86b24031832884fdd4615e9891ca2
Drew Jaynes <drew@affiliatewp.com>
2017-06-02 08:44:23 -0600

    Add Total Paid Earnings tiles (date based and all time).

--- includes/admin/reports/tabs/class-payouts-reports-tab.php
+++ includes/admin/reports/tabs/class-payouts-reports-tab.php
@@ -28,17 +28,65 @@ class Tab extends Reports\Tab {
 	}
 
 	/**
-	 * Registers the 'Total Payouts' (all time) tile.
+	 * Registers the 'Total Earnings Paid' (all time) tile.
 	 *
 	 * @access public
 	 * @since  2.1
 	 *
 	 * @see register_tile()
 	 */
-	public function total_payouts_tile() {
-		$this->register_tile( 'total_payouts', array(
-			'label'           => __( 'Total Payouts', 'affiliate-wp' ),
+	public function total_paid_all_time_tile() {
+		$payouts = affiliate_wp()->affiliates->payouts->get_payouts( array(
+			'number' => -1,
+			'fields' => 'amount',
+		) );
+
+		$this->register_tile( 'total_paid_all_time', array(
+			'label'           => __( 'Total Earnings Paid', 'affiliate-wp' ),
+			'type'            => 'amount',
+			'context'         => 'primary',
+			'data'            => array_sum( $payouts ),
+			'comparison_data' => __( 'All Time', 'affiliate-wp' ),
+		) );
+	}
+
+	/**
+	 * Registers the 'Total Earnings Paid' date-based tile.
+	 *
+	 * @access public
+	 * @since  2.1
+	 *
+	 * @see register_tile()
+	 */
+	public function total_earnings_paid_tile() {
+		$payouts = affiliate_wp()->affiliates->payouts->get_payouts( array(
+			'number' => -1,
+			'fields' => 'amount',
+			'date'   => $this->date_query,
+		) );
+
+		$this->register_tile( 'total_earnings_paid', array(
+			'label' => __( 'Total Earnings Paid', 'affiliate-wp' ),
+			'type'  => 'amount',
+			'context' => 'secondary',
+			'data'    => array_sum( $payouts ),
+			'comparison_data' => $this->get_date_comparison_label(),
+		) );
+	}
+
+	/**
+	 * Registers the 'Total Payouts Count' (all time) tile.
+	 *
+	 * @access public
+	 * @since  2.1
+	 *
+	 * @see register_tile()
+	 */
+	public function total_payouts_count_tile() {
+		$this->register_tile( 'total_payouts_count', array(
+			'label'           => __( 'Total Payouts Count', 'affiliate-wp' ),
 			'type'            => 'number',
+			'context'         => 'tertiary',
 			'data'            => affiliate_wp()->affiliates->payouts->count(),
 			'comparison_data' => __( 'All Time', 'affiliate-wp' ),
 		) );
@@ -79,7 +127,9 @@ class Tab extends Reports\Tab {
 	 * @since  2.1
 	 */
 	public function register_tiles() {
-		$this->total_payouts_tile();
+		$this->total_paid_all_time_tile();
+		$this->total_earnings_paid_tile();
+		$this->total_payouts_count_tile();
 		$this->average_payout_tile();
 	}
 
