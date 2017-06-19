28b8a018bbad7d0a1bfb78b84fd8e5cc9f1c04ec
Drew Jaynes <drew@affiliatewp.com>
2017-06-18 22:20:49 -0600

    Copyright notice in date-functions.php.

--- includes/date-functions.php
+++ includes/date-functions.php
@@ -4,7 +4,7 @@
  *
  * @package     AffiliateWP
  * @subpackage  Functions/Formatting
- * @copyright   Copyright (c) 2014, Pippin Williamson
+ * @copyright   Copyright (c) 2017, AffiliateWP, LLC
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       2.2
  */
