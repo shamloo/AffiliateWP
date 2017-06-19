34f0404a87609e0118b9871451d15d0a1c544b2f
Drew Jaynes <drew@affiliatewp.com>
2017-06-19 10:00:30 -0600

    Convert affiliate registrations graph to use date helpers.

--- includes/admin/reports/class-registrations-graph.php
+++ includes/admin/reports/class-registrations-graph.php
@@ -23,32 +23,26 @@ class Affiliate_WP_Registrations_Graph extends Affiliate_WP_Graph {
 	 */
 	public function get_data() {
 
-		$dates = affwp_get_report_dates();
-
-		$start = $dates['year'] . '-' . $dates['m_start'] . '-' . $dates['day'] . ' 00:00:00';
-		$end   = $dates['year_end'] . '-' . $dates['m_end'] . '-' . $dates['day_end'] . ' 23:59:59';
-		$date  = array(
-			'start' => $start,
-			'end'   => $end
-		);
+		$dates      = affwp_get_filter_dates();
+		$date_range = affwp_get_filter_date_range();
 
 		$affiliates = affiliate_wp()->affiliates->get_affiliates( array(
 			'orderby'  => 'date_registered',
 			'order'    => 'ASC',
 			'number'   => -1,
-			'date'     => $date,
+			'date'     => $dates,
 			'fields'   => 'date_registered',
 		) );
 
 		$affiliate_data = array();
-		$affiliate_data[] = array( strtotime( $start ) * 1000 );
-		$affiliate_data[] = array( strtotime( $end ) * 1000 );
+		$affiliate_data[] = strtotime( $dates['start'] ) * 1000;
+		$affiliate_data[] = strtotime( $dates['end'] ) * 1000;
 
 		if( $affiliates ) {
 
 			foreach( $affiliates as $affiliate_date_registered ) {
 
-				if( 'today' == $dates['range'] || 'yesterday' == $dates['range'] ) {
+				if( 'today' == $date_range || 'yesterday' == $date_range ) {
 
 					$point = strtotime( $affiliate_date_registered ) * 1000;
 
