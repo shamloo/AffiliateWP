eee13b44d9a71d047d1a49e4625bd1237e453405
Drew Jaynes <drew@affiliatewp.com>
2017-06-28 18:40:41 -0600

    Introduce the private get_core_timezone() method, mostly for the benefit of tests, but also any customer who doesn't set the timezone.

--- includes/admin/utilities/class-affwp-date.php
+++ includes/admin/utilities/class-affwp-date.php
@@ -54,9 +54,9 @@ final class Date {
 	 * @since  2.2
 	 */
 	public function __construct() {
-		$this->timezone        = get_option( 'timezone_string' );
-		$this->date_format     = get_option( 'date_format' );
-		$this->time_format     = get_option( 'time_format' );
+		$this->timezone        = $this->get_core_timezone();
+		$this->date_format     = get_option( 'date_format', 'M j, Y' );
+		$this->time_format     = get_option( 'time_format', 'g:i a' );
 		$this->datetime_format = $this->date_format . ' ' . $this->time_format;
 	}
 
@@ -112,5 +112,42 @@ final class Date {
 		return $formatted;
 	}
 
+	/**
+	 * Attempts to retrieve the WP timezone, or if not set, the timezone derived from the gmt_offset.
+	 *
+	 * @access private
+	 * @since  2.2
+	 *
+	 * @return string Timezone string, or if all checks fail, default is 'UTC'.
+	 */
+	private function get_core_timezone() {
+
+		// Passing a $default value doesn't work for the timezeon_string option.
+		$timezone = get_option( 'timezone_string' );
+
+		/*
+		 * If the timezone isn't set, or rather was set to a UTC offset, core saves the value
+		 * to the gmt_offset option and leaves timezone_string empty – because that makes
+		 * total sense, obviously. ¯\_(ツ)_/¯
+		 *
+		 * So, try to use the gmt_offset to derive a timezone.
+		 */
+		if ( empty( $timezone ) ) {
+			// Try to grab the offset instead.
+			$gmt_offset = get_option( 'gmt_offset', 0 );
+
+			// Yes, core returns it as a string, so as not to confuse it with falsey.
+			if ( '0' !== $gmt_offset ) {
+				$timezone = timezone_name_from_abbr( '', (int) $gmt_offset * HOUR_IN_SECONDS, date( 'I' ) );
+			}
+
+			// If the offset was 0 or $timezone is still empty, just use 'UTC'.
+			if ( '0' === $gmt_offset || empty( $timezone ) ) {
+				$timezone = 'UTC';
+			}
+		}
+
+		return $timezone;
+	}
 
 }
